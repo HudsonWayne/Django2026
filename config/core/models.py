da576ad1b8f35b92c9ad1608da6b865bfce4df99
HudsonWayne <waynewaynebenhura@gmail.com>
2026-01-14 14:29:27 +0200

    initial commit

--- config/core/models.py
+++ config/core/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class User(models.Models):
     name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
+
