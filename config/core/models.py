b5d6cd24da9c134c04fa85e5f1e5d37fa039981d
HudsonWayne <waynewaynebenhura@gmail.com>
2026-01-15 09:33:08 +0200

    initial commit

--- config/core/models.py
+++ config/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Models):
+class User(models.Model):
     name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     
