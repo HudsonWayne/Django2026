23c0b2f4e6b3574c5f2ae7906cc157baf674d5e4
HudsonWayne <waynewaynebenhura@gmail.com>
2026-01-15 10:01:12 +0200

    initial commit

--- config/core/views.py
+++ config/core/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-from django.http import HttpResponse
+from .models import User
 
 def home(request):
-    user = [
-        {"name": "Wayne", "is_active": True},
-        {"name": "Bob", "is_active": False},
-        {"name": "Charlie", "is_active": True},
-        
-    ]
-    return render(request, 'core/home.html', {"users": user})
+    users = User.objects.all()  # fetch all users from DB
+    return render(request, 'core/home.html', {"users": users})
