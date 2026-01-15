8953c348914fdac4da132673222d483d38b45fd3
HudsonWayne <waynewaynebenhura@gmail.com>
2026-01-15 10:10:19 +0200

    initial

--- config/core/views.py
+++ config/core/views.py
@@ -4,3 +4,6 @@ from .models import User
 def home(request):
     users = User.objects.all()  # fetch all users from DB
     return render(request, 'core/home.html', {"users": users})
+
+
+def create_user(request):
