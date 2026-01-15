4ea5c930d22aec364a113f224f9677decb54a084
HudsonWayne <waynewaynebenhura@gmail.com>
2026-01-15 10:25:23 +0200

    initial

--- config/core/urls.py
+++ config/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, create_user
 
 urlpatterns = [
-    path('', home, name = 'home')
+    path('', home, name='home'),
+    path('create/', create_user, name='create_user'),
 ]
